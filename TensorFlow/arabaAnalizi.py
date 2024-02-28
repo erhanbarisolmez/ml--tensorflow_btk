d1bc71524a897af5d5b0b6cae3a8ef290b58f873
erhanbarisolmez <61422236+erhanbarisolmez@users.noreply.github.com>
2024-02-28 17:27:54 +0300

    28.2.2024

--- TensorFlow/arabaAnalizi.py
+++ TensorFlow/arabaAnalizi.py
@@ -5,3 +5,19 @@ import seaborn as sbn
 
 dataFrame = pd.read_excel("merc.xlsx")
 dataFrame.head()
+
+dataFrame.describe() #all
+
+dataFrame.isnull().sum() # column toplam eksik veri
+
+
+plt.Figure(figsize=(7,5))
+sbn.displot(dataFrame["price"]) # nasıl dağıldığı
+
+sbn.countplot(dataFrame["year"])  # kaçtane olduğu
+
+numerical_columns = dataFrame.select_dtypes(include=[np.number]) # sayısal sütunları seçme
+
+correlation = numerical_columns.corr() # korelasyon hesaplama
+
+price_correlation = correlation["price"].sort_values() #"price" sğtunundaki diğer sütunlarla olan korelasyonu kontrol etme
